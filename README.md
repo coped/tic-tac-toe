92eb1cd42f661544f40347cb4bf029ee4a9440c6
Dennis Cope <dennisaaroncope@gmail.com>
2019-04-06 02:04:52 -0700

    Additions to game main body

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 This is a two-player, command line based game of tic-tac-toe made with Ruby. You can play by visiting this [link](https://repl.it/@coped/tic-tac-toe), or if you have Ruby installed, you can download this repository and play from the comfort of your own terminal.
 
 >"Wow, this is the best tic-tac-toe game I’ve ever played"
->
 >-Aaron W., uncoerced
 
 ### Rules
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,18 +1,19 @@
 class Player
-    attr_reader :name
-
+    attr_reader :name, :symbol
+    
     def initialize name, symbol
         @name = name
+        @symbol = symbol
     end
-
+    
     def turn
-        puts "#{@name}'s turn (#{@symbol})."
+        puts "#{@name}'s turn (#{@symbol}):"
         input = gets.chomp
     end
 end
 
-class Game
-    attr_accessor :board, :victory
+class GameBoard < Player
+    attr_accessor :board, :victory, :board_positions, :one
 
     def initialize 
         @one = "1"
@@ -26,9 +27,11 @@ class Game
         @nine = "9"
 
         @victory = false
-
-        
     end
+
+    
+
+    @board_positions = [@one, @two, @three, @four, @five, @six, @seven, @eight, @nine]
 
     @@winning_rows = [
         [@one, @two, @three],
@@ -45,62 +48,79 @@ class Game
 
     def board_view
         puts ""
-        puts " #{@one} | #{@two} | #{@three} "
-        puts "-----------"
-        puts " #{@four} | #{@five} | #{@six} "
-        puts "-----------"
-        puts " #{@seven} | #{@eight} | #{@nine} "
+        puts " #{@one} | #{@two} | #{@three} ".center(100)
+        puts "---|---|---".center(100)
+        puts " #{@four} | #{@five} | #{@six} ".center(100)
+        puts "---|---|---".center(100)
+        puts " #{@seven} | #{@eight} | #{@nine} ".center(100)
     end
 
     def check_victory 
-        winning_rows.each do |row|
+        @@winning_rows.each do |row|
             if row.all? {|i| i === "X"} || row.all? {|i| i === "O"}
                 @victory = true
             end
         end
     end
-
-
 end
+
 puts "------------------------------------------------------------------".center(100)
 puts ""
 puts "WELCOME TO TIC-TAC-TOE".center(100)
 puts ""
 puts "------------------------------------------------------------------".center(100)
 
-replay = true
-until replay == "no"
+replay = "yes"
+while replay == "yes"
     puts "\nEnter Player 1's Name (X):"
-    player_1_name = gets.chomp
+    player_1_name = gets.chomp.strip
     player_1 = Player.new player_1_name, "X"
 
     puts "\nEnter Player 2's Name (O):"
-    player_2_name = gets.chomp
+    player_2_name = gets.chomp.strip
     player_2 = Player.new player_2_name, "O"
 
-    puts "---------------------------".center(100)
-    puts ""
-    puts "READY TO PLAY? (yes/no)".center(100)
-    puts ""
-    puts "---------------------------".center(100)
-    ready = gets.chomp.downcase
-    until ready == "yes"
-        puts "-----------------------------------".center(100)
-        puts ""
-        puts "... ARE YOU READY NOW? (yes/no)".center(100)
-        puts ""
-        puts "-----------------------------------".center(100)
-        ready = gets.chomp.downcase
-    end
+    # puts "---------------------------".center(100)
+    # puts ""
+    # puts "READY TO PLAY? (yes/no)".center(100)
+    # puts ""
+    # puts "---------------------------".center(100)
 
-    new_game = Game.new
+    # ready = gets.chomp.downcase
+    # until ready == "yes"
+    #     ["\n\t\t\t\t\t\t.", ".", "."].each {|i| print i; sleep 1}
+    #     2.times {puts ""}
+    #     puts "-----------------------------------".center(100)
+    #     puts ""
+    #     puts "... ARE YOU READY NOW? (yes/no)".center(100)
+    #     puts ""
+    #     puts "-----------------------------------".center(100)
+    #     ready = gets.chomp.downcase
+    # end
+
+    new_game = GameBoard.new
+    puts new_game.board_view
 
     while new_game.victory == false
-        # Main game body will go here
+        player_1.turn
+        new_game.one = player_1.symbol
+        puts new_game.board_view
+        new_game.check_victory
+
+        break if new_game.victory
+
+        player_2.turn
+        puts new_game.board_view
+        new_game.check_victory
     end
 
     puts "Congratulations, whoever won."
     puts "Replay? (yes/no)"
     replay = gets.chomp.downcase
 end
+puts "-----------------------------------".center(100)
+puts ""
+puts "THANKS FOR PLAYING".center(100)
+puts ""
+puts "-----------------------------------".center(100)
 
